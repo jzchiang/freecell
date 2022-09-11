0fcbc4b3059fe8021820667d9576928070a09bef
Jonathan Chiang <jonathan.z.chiang@gmail.com>
2022-09-11 17:59:22 -0400

    Autoformat

--- tests/tests.py
+++ tests/tests.py
@@ -2,13 +2,16 @@ import unittest
 
 from .context import freecell
 
+
 class TestValue(unittest.TestCase):
     def test_number_of_values(self):
         self.assertEqual(len([x for x in freecell.Value]), 13)
 
+
 class TestSuit(unittest.TestCase):
     def test_number_of_values(self):
         self.assertEqual(len([x for x in freecell.Suit]), 4)
+
 
 class TestCard(unittest.TestCase):
     def test_color_match_suit_as(self):
@@ -16,7 +19,7 @@ class TestCard(unittest.TestCase):
         suit = freecell.Suit.SPADES
         expected_color = freecell.Color.BLACK
         self.check_color_match_suit(value, suit, expected_color)
-    
+
     def test_color_match_suit_kh(self):
         value = freecell.Value.kK
         suit = freecell.Suit.HEARTS
@@ -28,7 +31,7 @@ class TestCard(unittest.TestCase):
         suit = freecell.Suit.CLUBS
         expected_color = freecell.Color.BLACK
         self.check_color_match_suit(value, suit, expected_color)
-    
+
     def test_color_match_suit_jd(self):
         value = freecell.Value.kJ
         suit = freecell.Suit.DIAMONDS
@@ -38,6 +41,7 @@ class TestCard(unittest.TestCase):
     def check_color_match_suit(self, value, suit, expected_color):
         card = freecell.Card(value, suit)
         self.assertEqual(card.color, expected_color)
+
 
 class TestBoard(unittest.TestCase):
     def test_number_of_cards(self):
